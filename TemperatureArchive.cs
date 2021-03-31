977f4fb20402681428fa6d96aa2cba612dc14c20
Adam Lapčák <xlapca00@vutbr.cz>
2021-03-31 16:44:44 +0200

    Přidejte soubory projektu.

---
+++ TemperatureArchive.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cv8
+{
+    class TemperatureArchive
+    {
+        private SortedDictionary <double, YearTemperature> _archive;
+
+        public void Load(string path)
+        {
+            StreamReader reader = File.OpenText(path);
+            _archive = new SortedDictionary <double, YearTemperature>();
+
+            while (reader.ReadLine() != null)
+            {
+                int year = 0;
+                string line = reader.ReadLine();
+                List<double> temperatures = new List<double>();
+
+                line = line.Replace(":", "");
+                List<string> values = line.Split('\t').ToList();
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        year = Convert.ToInt32(values[i]);
+                    }
+                    else
+                    {
+                        temperatures.Add(Convert.ToDouble(values[i]));
+                    }
+                }
+
+                _archive.Add(year, new YearTemperature(year, temperatures));
+            }
+        }
+        public void Save(string path)
+        {
+            StreamWriter writer = File.CreateText(path);
+
+            foreach (var item in _archive.Values)
+            {
+                writer.Write("{0}:\t", item.Year);
+
+                foreach (double temperature in item.MonthTemperatures)
+                {
+                    writer.Write("{0:0.0}\t", temperature);
+                }
+
+                writer.WriteLine();
+            }
+
+            writer.Close();
+        }
+
+        public void Calibration(double number)
+        {
+            foreach (var item in _archive.Values)
+            {
+                for (int i = 0; i < item.MonthTemperatures.Count; i++)
+                {
+                    item.MonthTemperatures[i] = item.MonthTemperatures[i] + number;
+                }
+            }
+        }
+
+        public void Search(int y)
+        {
+            if (_archive.ContainsKey(y))
+            {
+                Console.WriteLine("{0}: ", y);
+
+                foreach (double temperature in _archive[y].MonthTemperatures)
+                {
+                    Console.WriteLine("{0:0.0}; ", temperature);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Year not found");
+            }   
+        }
+
+        public void PrintTemperatures()
+        {
+            foreach (var item in _archive.Values)
+            {
+                Console.WriteLine("{0}:\t", item.Year);
+
+                foreach (double temperature in item.MonthTemperatures)
+                {
+                    Console.WriteLine("{0:0.0}\t", temperature);
+                }
+            }
+        }
+
+        public void PrintAnnualAverageTemperature()
+        {
+            foreach (var item in _archive.Values)
+            {
+                Console.WriteLine("{0}: {1:0.0}", item.Year, item.AnnualAvgTemperature);
+            }
+        }
+
+        public void PrintMonthlyAverageTemperature()
+        {
+            List<double> AvgMonthTemp = new List<double>();
+            
+            for (int i = 0; i < 12; i++)
+            {
+                AvgMonthTemp.Add(0);
+            }
+
+            foreach (var item in _archive.Values)
+            {
+                for (int i = 0; i < item.MonthTemperatures.Count; i++)
+                {
+                    AvgMonthTemp[i] = AvgMonthTemp[i] + item.MonthTemperatures[i];
+                }
+            }
+
+            Console.WriteLine("AvgMothlyTemperature:\t");
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine("{0:0.0}\t", AvgMonthTemp[i] / _archive.Keys.Count);
+            }
+        }
+    }
+}
---
+++ YearTemperature.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv8
+{
+    class YearTemperature
+    {
+        public int Year { get; private set; }
+
+        public List<double> MonthTemperatures;
+
+        public double MaxTemperature
+        {
+            get
+            {
+                return MonthTemperatures.Max();
+            }
+            private set
+            {
+
+            }
+        }
+        public double MinTemperature
+        {
+            get
+            {
+                return MonthTemperatures.Min();
+            }
+            private set
+            {
+
+            }
+        }
+
+        public double AnnualAvgTemperature
+        {
+            get
+            {
+                double temporary = 0.0;
+
+                foreach (double temperature in MonthTemperatures)
+                {
+                    temporary = temporary + temperature;
+                }
+
+                return temporary/12.0;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public YearTemperature(int y, List<double> t)
+        {
+            Year = y;
+            MonthTemperatures = new List<double>();
+            MonthTemperatures = t;
+        }
+    }
+}
